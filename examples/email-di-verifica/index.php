11b3bb9db0edb512ba30d87c030a995b55af816b
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-01-30 13:13:17 +0100

    Creato il file index.php

---
+++ examples/email-di-verifica/index.php
@@ -0,0 +1,16 @@
+<?php
+include("loader.php");
+?>
+<!DOCTYPE html>
+<html>
+ <header>
+  <title>HPB - Test</title>
+ </header>
+ <body>
+  <br><br><br>
+  <h1>HPB - Invia e verifica un codice tramite Email</h1>
+  <br><br>
+  <br>
+  <a href="verifica.php"><button>Verifica la tua Email!</button></a>
+ </body>
+</html>
