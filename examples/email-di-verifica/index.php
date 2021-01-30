e75e708d20aec10940bb29cda9bc4a1cf77ae02c
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-01-30 13:16:50 +0100

    Aggiornato il file index.php

--- examples/email-di-verifica/index.php
+++ examples/email-di-verifica/index.php
@@ -11,6 +11,10 @@ include("loader.php");
   <h1>HPB - Invia e verifica un codice tramite Email</h1>
   <br><br>
   <br>
-  <a href="verifica.php"><button>Verifica la tua Email!</button></a>
+   <form method="POST" action="verifica.php">
+        Inserisci la tua Email:<br>
+        <input type="text" name="email"><br><br>
+        <input type="submit" value="Verifica!">
+   </form>
  </body>
 </html>
