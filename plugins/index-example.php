022e9ee21708d67e76b3963490b50ec36a51619c
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-03-04 21:38:51 +0100

    Creato un esmepio di una pagina che carica i plugin

---
+++ plugins/index-example.php
@@ -0,0 +1,14 @@
+<?php
+// Includo il Loader
+include("loader.php")
+
+// Indico di caricare il plugin "curl"
+plugin("load", "curl");
+
+link("https://mystudents.ml/projects/FoxWorn3365/hpb/");
+  
+stampa($output);
+
+pulisci();
+
+?>
